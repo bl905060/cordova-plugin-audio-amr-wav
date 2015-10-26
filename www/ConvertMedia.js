46a6f6af05ebae634530cbfd324ac353cdde0419
Casper <bilei905060@icloud.com>
2015-10-26 13:45:55 +0800

    test

--- www/ConvertMedia.js
+++ www/ConvertMedia.js
@@ -1,9 +1,37 @@
 module.exports = {
-    greet: function(name, successCallback, errorCallback) {
-        cordova.exec(successCallback,
+    record: function(param, successCallback, errorCallback) {
+        /*cordova.exec(successCallback,
                      errorCallback,
                      "ConvertMedia",
-                     "greet",
-                     [name]);
+                     "startRecord",
+                     [param]);*/
+        alert(1);
+    }
+    
+    stop: function(param, successCallback, errorCallback) {
+        /*cordova.exec(successCallback,
+                     errorCallback,
+                     "ConvertMedia",
+                     "stopRecord",
+                     [param]);*/
+        alert(2);
+    }
+    
+    play: function(audioURL, successCallback, errorCallback) {
+        /*cordova.exec(successCallback,
+                     errorCallback,
+                     "ConvertMedia",
+                     "playAudio",
+                     [audioURL]);*/
+        alert(3);
+    }
+    
+    convertToAmr: function(audioURL, successCallback, errorCallback) {
+        /*cordova.exec(successCallback,
+                     errorCallback,
+                     "ConvertMedia",
+                     "convertToAmr",
+                     [audioURL]);*/
+        alert(4);
     }
 };
